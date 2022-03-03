30a430fcb01a270899cf9d990d760fa79e44e017
Alireza Davoodi <alirezadavoodi1378@gmail.com>
2022-03-03 20:07:12 +0330

    backend.py

---
+++ backend.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from threading import Thread
+from credentials import port
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return "Bot is alive."
+
+
+@app.route('/bot_name')
+def show_bot_name():
+    return "Social Credit Manager"
+
+
+def run():
+    print(f"[INFO]: App is up on port {port}")
+    app.run(host='0.0.0.0', port=port)
+
+
+def keep_alive():
+    t1 = Thread(target=run)
+    t1.start()
+    # t2 = Thread(target=run_pre_commands)
+    # t2.start()
