cd68d1b0ff16d30bd98cb9c40675e90a7c4afd70
Alireza Davoodi <alirezadavoodi1378@gmail.com>
2022-03-05 05:50:20 +0330

    fix

--- credentials.py
+++ credentials.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 bot_token = os.getenv('bot_token')
-db_url = os.getenv('db_url')
+db_url = os.getenv('DATABASE_URL')
 port = os.environ['PORT']
 sunset_sv_status_msg_link = "https://discord.com/api/v9/channels/926148025007607908/messages?limit=50" # server-status
 mc_bot_id = 936315743216218203
