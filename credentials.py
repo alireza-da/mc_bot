1e1d7f073f44933e7a5adf91ab2b5912af164fa6
Alireza Davoodi <alirezadavoodi1378@gmail.com>
2022-04-10 19:15:42 +0430

    Star Menu

--- credentials.py
+++ credentials.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# bot_token = os.getenv('bot_token')
+bot_token = os.getenv('bot_token')
 db_url = os.getenv('DATABASE_URL')
-db_url = os.getenv('db_url')
+# db_url = os.getenv('db_url')
 port = os.environ['PORT']
 sunset_sv_status_msg_link = "https://discord.com/api/v9/channels/926148025007607908/messages?limit=5"  # server-status
 mc_bot_id = 936315743216218203
